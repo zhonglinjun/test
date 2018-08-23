1ea495f4be717f36788d9e16d0492e5772d78874
liubojianggit <42627270+liubojianggit@users.noreply.github.com>
2018-08-23 09:16:25 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yr.service.UserService;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+		UserService userService = (UserService) ctx.getBean("userServiceImplement");
+		userService.deleteUser("2");
+	}
+}
