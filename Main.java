cae270c8dd4b9b364b5303c8b91e22ce20bdc458
zhonglinjun <zhonglinjun@users.noreply.github.com>
2018-08-23 15:55:04 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,8 +9,8 @@ public class Main {
 		
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
 		UserService userService = (UserService) ctx.getBean("userServiceImplement");
-		userService.deleteUser("------------------------------");
-		userService.deleteUser("------------------------------");
+		userService.deleteUser("------------------------------11");
+		userService.deleteUser("------------------------------11");
 		userService.deleteUser("=============================12312312");
 	}
 }
